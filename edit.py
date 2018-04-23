d82ce2aa51011a2c0fd84fbef0ac859ceb80d511
L-Andrew <chunyenl+github@gmail.com>
2018-04-23 15:50:52 -0700

    changed collageV and collageH added filterGreyscale, filterR
    Signed-off-by: L-Andrew <chunyenl+github@gmail.com>

--- edit.py
+++ edit.py
@@ -18,8 +18,8 @@ def chromakey(source, bg):
             if distance(cur_pixel, white) < 250:
                 # grab the color at the same spot from the new background
                 source.putpixel((x,y), bg.getpixel((x,y)))
-    source.save("result.png")
     source.show()
+    return source
 
 def crop(source, x1, y1, x2, y2):
     if (x1 != x2 and y1 != y2):
@@ -28,8 +28,9 @@ def crop(source, x1, y1, x2, y2):
         for x in range(min(x1, x2), min(source.width, max(x1, x2))):
             for y in range(min(y1, y2), min(source.height, max(y1, y2))):
                 new_img.putpixel((x - min(x1, x2), y - min(y1, y2)), source.getpixel((x, y)))
-        new_img.save("result.png")
-        new_img.show()
+        # new_img.save("result.png")
+        # new_img.show()
+        return new_img
     else:
         print("x1 == x2 || y1 == y2")
 
@@ -43,30 +44,68 @@ def resize(source, percent):
     #new_img.save("result.png")
     return new_img
 
-def collageH(source1, source2):
-    resized = resize(source2, source1.height/source2.height)
-    new_img = Image.new("RGB", (source1.width + resized.width, source1.height))
-    for x in range(source1.width):
-        for y in range(source1.height):
-            new_img.putpixel((x, y), source1.getpixel((x, y)))
-    for x in range(resized.width):
-        for y in range(resized.height):
-            new_img.putpixel((source1.width + x, y), resized.getpixel((x, y)))
-    new_img.show()
+def collageH(source, *sourceList):
+    totalX = 0
+    sList = list(sourceList)
+    for i, img in enumerate(sList):
+        if (img.height != source.height):
+            img = resize(img, source.height/img.height)
+            sList[i] = img
+        totalX = totalX + img.width
+    new_img = Image.new("RGB", (source.width + totalX, source.height))
+    for x in range(source.width):
+        for y in range(source.height):
+            new_img.putpixel((x, y), source.getpixel((x, y)))
+    offsetX = source.width
+    for img in sList:
+        for x in range(img.width):
+            for y in range(img.height):
+                new_img.putpixel((offsetX + x, y), img.getpixel((x, y)))
+        offsetX = offsetX + img.width
+    # new_img.show()
+    return new_img
 
-def collageV(source1, source2):
-    resized = resize(source2, source1.width/source2.width)
-    new_img = Image.new("RGB", (source1.width, source1.height + resized.height))
-    for x in range(source1.width):
-        for y in range(source1.height):
-            new_img.putpixel((x, y), source1.getpixel((x, y)))
-    for x in range(resized.width):
-        for y in range(resized.height):
-            new_img.putpixel((x, source1.height + y), resized.getpixel((x, y)))
-    new_img.show()
+def collageV(source, *sourceList):
+    totalY = 0
+    sList = list(sourceList)
+    for i, img in enumerate(sList):
+        if (img.width != source.width):
+            img = resize(img, source.width/img.width)
+            sList[i] = img
+        totalY = totalY + img.height
+    new_img = Image.new("RGB", (source.width, source.height + totalY))
+    for x in range(source.width):
+        for y in range(source.height):
+            new_img.putpixel((x, y), source.getpixel((x, y)))
+    offsetY = source.height
+    for img in sList:
+        for x in range(img.width):
+            for y in range(img.height):
+                new_img.putpixel((x, offsetY + y), img.getpixel((x, y)))
+        offsetY = offsetY + img.height
+    # new_img.show()
+    return new_img
 
-#crop(img1, int(img1.width*.2), int(img1.height*.2), int(img1.width*.8), int(img1.height*.8))
-#resize(img1, 0.5)
-#resize(img1, 2)
-#collageH(img1, img2)
-collageV(img1, img2)
+def filterGrayscale(source):
+    new_img = source.convert("L")
+    # new_img.show()
+    return new_img
+
+def filterR(source, percent):
+    for x in range(source.width):
+        for y in range(source.height):
+            r, g, b = source.getpixel((x, y))
+            r = int(r * percent)
+            source.putpixel((x,y), (r, g, b))
+    source.show()
+    return source
+
+# crop(img1, int(img1.width*.2), int(img1.height*.2), int(img1.width*.8), int(img1.height*.8))
+# resize(img1, 0.5)
+# resize(img1, 2)
+# collageH(img1, img2)
+# collageV(img1, img2)
+# filterGrayscale(img1)
+# collageH(img1, img1, img2, img1)
+# collageV(img1, img1, img2, img1)
+filterR(img1, 3)
